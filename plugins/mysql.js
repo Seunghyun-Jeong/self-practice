6d281e42a2ad5e49dd44780be2d9315561c9440d
Seunghyun-Jeong <95638057+Seunghyun-Jeong@users.noreply.github.com>
2022-07-24 22:48:32 +0900

    Update mysql.js

--- plugins/mysql.js
+++ plugins/mysql.js
@@ -3,8 +3,8 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(async function (fastify, opts) {
-  fastify.register(require('@fastify/mysql'), {
-    promise: true,
-    connectionString: `mysql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOSTNAME}/mydb`
-  })
+//   fastify.register(require('@fastify/mysql'), {
+//     promise: true,
+//     connectionString: `mysql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOSTNAME}/mydb`
+//   })
 })
