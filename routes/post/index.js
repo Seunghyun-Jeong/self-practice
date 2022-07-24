b7bf35e2573ff4cb2314a24e16d99384eca91c60
Seunghyun-Jeong <dkskakdgka@gmail.com>
2022-07-24 22:50:59 +0900

    disable mysql

--- routes/post/index.js
+++ routes/post/index.js
@@ -4,12 +4,14 @@ const message = []
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    const connection = await fastify.mysql.getConnection()
-    const [rows, fields] = await connection.query(
-        'SELECT * FROM users', [],
-    )
-    connection.release()
-    return rows[0]
+    // const connection = await fastify.mysql.getConnection()
+    // const [rows, fields] = await connection.query(
+    //     'SELECT * FROM users', [],
+    // )
+    // connection.release()
+    // return rows[0]
+
+    reply.send(message)
   })
 
   fastify.post('/', async function (request, reply) {
